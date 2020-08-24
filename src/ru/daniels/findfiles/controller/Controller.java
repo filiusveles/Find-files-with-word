053b193bee64d64e1483af26d012faef4dccbe3b
Daniel <denhif@yandex.com>
2020-08-24 22:08:22 +0300

    Refactor class FileVisit, divided the file search and GUI into threads

--- src/ru/daniels/findfiles/controller/Controller.java
+++ src/ru/daniels/findfiles/controller/Controller.java
@@ -1,13 +1,13 @@
 package ru.daniels.findfiles.controller;
 
+import com.sun.istack.internal.NotNull;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.scene.control.*;
 import javafx.stage.DirectoryChooser;
-import ru.daniels.findfiles.utils.FileVisit;
+import ru.daniels.findfiles.utils.FileSearcher;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
@@ -16,28 +16,25 @@ public class Controller {
     public TextField fieldKeyword;
     public TextField fieldExtension;
     public TreeView<String> fileTree;
+    public ScrollPane filesScrollPane;
+    public Label pathLabel;
+    public Label keywordLabel;
 
-
+    private String path;
     private String keyword;
     private String extension = ".log";
 
 
     public void startSearch() {
-        File file = new File(fieldPathToFile.getText());
-        String name = file.getName();
-        name = name.equals("") ? file.toString() : name;
-        TreeItem<String> root = new TreeItem<>(name);
-        keyword = fieldKeyword.getText();
-        fileTree.setRoot(createTree(root, files()));
-        fileTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
-                System.out.println("name: " + newValue.getValue() + ", leaf: " + newValue.isLeaf()));
+        if(checkValues()) showProgressWhenWait();
     }
 
     public void showExplorer() {
         DirectoryChooser chooser = new DirectoryChooser();
         File dir = chooser.showDialog(null);
         if(dir != null) {
-            fieldPathToFile.setText(dir.toString());
+            path = dir.toString();
+            fieldPathToFile.setText(path);
         }
     }
 
@@ -63,22 +60,56 @@ public class Controller {
                 start = newItem;
             }
         }
+        fileTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+                System.out.println("name: " + newValue.getValue() + ", leaf: " + newValue.isLeaf()));
         return root;
     }
 
-    private List<Path> files(){
-        List<Path> files = new LinkedList<>();
-        File root = new File(fieldPathToFile.getText());
-        try {
-            Files.walkFileTree(root.toPath(), new FileVisit(files, keyword, extension));
-        } catch (IOException exception) {
-            exception.printStackTrace();
+    private boolean checkValues(){
+        keyword = fieldKeyword.getText();
+        path = path == null ? fieldPathToFile.getText() : path;
+        extension = fieldExtension.getText().equals("") ? this.extension : fieldExtension.getText();
+
+        if(path.equals("") && keyword.equals("")){
+            missingValuesAlert(pathLabel.getText(), keywordLabel.getText());
+            return false;
         }
-        return files;
+        else if(path.equals("")) {
+            missingValuesAlert(pathLabel.getText());
+            return false;
+        }
+        else if(keyword.equals("")) {
+            missingValuesAlert(keywordLabel.getText());
+            return false;
+        }
+        return true;
     }
 
+    private void missingValuesAlert(String... values){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Missing Value");
+        alert.setHeaderText(null);
+        alert.setContentText("Missing values: " + Arrays.toString(values));
+        alert.showAndWait();
 
+    }
 
+    private void showProgressWhenWait(){
+        FileSearcher searcher = new FileSearcher(path, keyword, extension);
+        ProgressBar progressBar = new ProgressBar();
+        progressBar.progressProperty().bind(searcher.progressProperty());
+        filesScrollPane.setContent(progressBar);
+        File file = new File(path);
+        String name = file.getName();
+        name = name.equals("") ? file.toString() : name;
+        TreeItem<String> root = new TreeItem<>(name);
+
+        searcher.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, event -> {
+            fileTree.setRoot(createTree(root, searcher.getValue()));
+            filesScrollPane.setContent(fileTree);
+        });
+        new Thread(searcher).start();
+    }
 
 
 
---
+++ src/ru/daniels/findfiles/utils/FileSearcher.java
@@ -0,0 +1,82 @@
+package ru.daniels.findfiles.utils;
+
+import com.sun.istack.internal.NotNull;
+import javafx.concurrent.Task;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FileSearcher extends Task<List<Path>> {
+    private final String extension;
+    private final String path;
+    private final String keyword;
+
+    public FileSearcher(@NotNull String path, String keyword, String extension){
+        this.path = path;
+        this.keyword = keyword;
+        this.extension = extension;
+    }
+
+
+    @Override
+    protected List<Path> call() {
+        return files();
+    }
+
+    private List<Path> files(){
+        List<Path> files = new LinkedList<>();
+        File root = new File(path);
+        try {
+            Files.walkFileTree(root.toPath(), new FileVisit(files));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return files;
+    }
+
+    class FileVisit extends SimpleFileVisitor<Path> {
+        private final List<Path> files;
+
+        public FileVisit(List<Path> files){
+            this.files = files;
+        }
+
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if(file.toString().endsWith(extension) && isWordContains(file.toUri()))
+                files.add(file);
+            return super.visitFile(file, attrs);
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            if(exc instanceof AccessDeniedException) return FileVisitResult.SKIP_SUBTREE;
+            return super.visitFileFailed(file, exc);
+        }
+
+        private boolean isWordContains(URI path){
+            byte[] bytes = keyword.getBytes();
+
+            try(FileReader reader = new FileReader(new File(path))) {
+                int c;
+                int counter = 0;
+                while ((c=reader.read())!=-1){
+                    if(counter == bytes.length-1) return true;
+                    if(c == bytes[counter]) counter++;
+                    else counter = 0;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+    }
+}
