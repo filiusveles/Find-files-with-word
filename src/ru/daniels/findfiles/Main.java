7b95bba7bbfb33a94689bac6c015cc8b84eebce0
Daniel <69090604+filiusveles@users.noreply.github.com>
2020-09-24 19:54:01 +0300

    Корректировка
    Скорректирован код

--- src/ru/daniels/findfiles/Main.java
+++ src/ru/daniels/findfiles/Main.java
@@ -33,46 +33,7 @@ public class Main extends Application {
 
     }
 
-
     public static void main(String[] args) {
-        //launch(args);
-
-        System.exit(0);
+        launch(args);
     }
-
-    public static int expandedForm(int num)
-    {
-        int left = 0;
-        int right = num;
-        while(left < right){
-            int mid = (left + right)/2;
-            int sqrtMid = mid * mid;
-            if(sqrtMid == num) return mid;
-            if(sqrtMid > num) right = mid;
-            if(sqrtMid < num) left = mid+1;
-        }
-        return left - 1;
-    }
-
-    public static String expandedForm1(int num)
-    {
-        String outs = "";
-        for (long i = 10; i < num; i *= 10) {
-            long rem = num % i;
-            outs = (rem > 0) ? " + " + rem + outs : outs;
-            num -= rem;
-        }
-        outs = num + outs;
-
-        return outs;
-    }
-
-
-
-
-
-
-
-
-
 }
